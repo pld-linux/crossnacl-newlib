0931a2eebc6f8d46f794d46a5612fc178f60c4b8
Elan Ruusamäe <glen@delfi.ee>
2012-11-11 11:33:16 +0200

    - use http url for omahaproxy

--- get-source.sh
+++ get-source.sh
@@ -16,7 +16,7 @@ set -e
 package=nacl-newlib
 repo_url=http://git.chromium.org/native_client/$package.git
 nacl_trunk=http://src.chromium.org/native_client/trunk
-omahaproxy_url=https://omahaproxy.appspot.com
+omahaproxy_url=http://omahaproxy.appspot.com
 specfile=crossnacl-newlib.spec
 
 # if you get errors that sha1 hash not found, try increasing depth
